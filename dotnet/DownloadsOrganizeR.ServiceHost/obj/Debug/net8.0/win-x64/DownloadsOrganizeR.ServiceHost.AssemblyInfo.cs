2244ca12b60a555ca64e0aaec830a7d8a8a136d3
Atomsk865 <rgdennett@gmail.com>
2025-12-03 01:47:56 +0000

    Dashboard layout: add sidebar-aware padding and responsive reset
    - Add `dashboard-root` class to base container
    - Apply `.dashboard-root { padding-left: var(--sidebar-width); }`
    - Reset padding on <= 992px screens to avoid overlap

--- dotnet/DownloadsOrganizeR.ServiceHost/obj/Debug/net8.0/win-x64/DownloadsOrganizeR.ServiceHost.AssemblyInfo.cs
+++ dotnet/DownloadsOrganizeR.ServiceHost/obj/Debug/net8.0/win-x64/DownloadsOrganizeR.ServiceHost.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DownloadsOrganizeR.ServiceHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4244be90548cfe7f2b43ab7e1219d454aeaa6c9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2cbdce842e482a9d7e83cea1dfcae37d58e1d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DownloadsOrganizeR.ServiceHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DownloadsOrganizeR.ServiceHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
