5bb83d448827f6d0f0e663a726fca3bc62638d0d
Atomsk865 <rgdennett@gmail.com>
2025-12-06 08:59:40 +0000

    Align config dashboard with GridStack layout

--- dotnet/DownloadsOrganizeR.ServiceHost/obj/Debug/net8.0/win-x64/DownloadsOrganizeR.ServiceHost.AssemblyInfo.cs
+++ dotnet/DownloadsOrganizeR.ServiceHost/obj/Debug/net8.0/win-x64/DownloadsOrganizeR.ServiceHost.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DownloadsOrganizeR.ServiceHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2b766742876789909989e339445c588b7dc2c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff6d08adc5e26808ca015b6312cdacf88f04fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("DownloadsOrganizeR.ServiceHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DownloadsOrganizeR.ServiceHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
